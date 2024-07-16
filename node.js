f2a5b7457244cfceff72149a6089d959ac0bf5c8
Corry <schaap.marco@gmail.com>
2024-07-17 00:02:46 +0200

    Clean up and added more commments

--- node.js
+++ node.js
@@ -1,5 +1,8 @@
-// This is the node class that can be seen as a peer in a peer to peer network
-// In basic it will be a peer that can connect to other peers.
+// This is a btc full-node class that can be seen as a peer in a peer to peer network
+// It basically is a very simple peer to peer network where nodes can connect/disconnect to/from the network.
+// When a node connects to another node, it sends a handshake message back. This way we can establish a two way connection.
+// When a node receives a broadcast message, it will broadcast the message to all other nodes in the network.
+
 // Later we will add the functionality to mine blocks and add them to the blockchain
 
 import {WebSocketServer, WebSocket} from 'ws';
@@ -13,6 +16,7 @@ class Node {
         this.server = new WebSocketServer({port: this.port});
         // keep array of connected peers ws server addresses
         this.serverAddresses = [];
+        this.messageIds = [];
 
         // if someone connects to the server, set up the connection
         this.server.on('connection', (ws, req) => {
@@ -22,10 +26,10 @@ class Node {
         console.log(`Node:${this.id} is running on port ` + this.port);
     }
 
+    // This function handles incomming connections to the node. It sets up event listeners to communicate with nodes that connected to us.
     setUpConnection(ws, req) {
         console.log(`Node:${this.id} connection with ${req.socket.remoteAddress}:${req.socket.remotePort}`);
         const id = `${req.socket.remoteAddress}:${req.socket.remotePort}`;
-        this.peers.set(id, ws);
 
         ws.on('message', (message) => {
             this.handleMessage(message, ws, id);
@@ -40,11 +44,16 @@ class Node {
             console.log(`Node:${this.id} error: ${error}`);
         });
 
-        // Send a handshake message with this peer's address
+        // When a peer connects to us, we send him a handshake message.
+        // maybe we can randomly send a list of peers to the new peer from our own list of peers.
         const handshakeMessage = JSON.stringify({ type: 'handshake', address: id, from: `ws://localhost:${this.port}`} );
         ws.send(handshakeMessage);
+
+        // And add the peer to the list of peers
+        this.peers.set(id, ws);
     }
 
+    // This function initiates outbound connections to other nodes. It sets up event listeners to communicate with target nodes.
     connectToPeer(address) {
         console.log(`Node:${this.id} connecting to ${address}`);
         // check if address is already in the list of server addresses
@@ -60,10 +69,9 @@ class Node {
         ws.on('open', () => {
             console.log(`Node:${this.id} Connected to peer: ${address}`);
             
-            // Send a handshake message with this peer's address
+            // Send a handshake-back message to the peer that we try to connect to
             const handshakeMessage = JSON.stringify({ type: 'handshake-back', address: `ws://localhost:${this.port}`, from: `ws://localhost:${this.port}` });
             ws.send(handshakeMessage);
-
         });
 
         ws.on('message', (message) => {
@@ -93,15 +101,50 @@ class Node {
     handleMessage(message, ws, id) {
         const data = JSON.parse(message);
         if (data.type === 'handshake') {
-            // when we receive a handshake message, add the peer to the list of peers
+            // This is where a peer receives the handshake message when it tries to connect to another peer
+            // when we receive a handshake message, we add the peer to the list of peers
             this.peers.set(data.address, ws);
         } else if (data.type === 'handshake-back') {
             // when we receive a handshake-back message, add the server address to the list of server addresses
+            // when a two way connection is established, we can send messages to the peer.
+            // maybe next step we send a copy from the blockchain
             this.serverAddresses.push(data.address);            
+        } else if (data.type === 'broadcast') {
+            // when we receive a broadcast message, check if we have already received this message
+            // if not, broadcast it to all peers
+            if (!this.isIdInMessageIds(data.id)) {
+                console.log(`Node ${this.id} - Received broadcast message from ${data.from}: ${data.message}`);
+
+                this.addMessageId(data.id);
+                this.broadcastMessage(data.id, data.message);
+            }
         } else {
           console.log(`Node ${this.id} - Received message from ${id}: ${message}`);
         }
       }
+
+    // Broadcast a message to all peers which will be send to all peers. This will make sure the message gets send to all the nodes in the network.
+    // Modes need to keep track of messages they already received to prevent infinite loops. 
+    broadcastMessage(id, message) {
+        // if i'm sending this message and the id is not in the list of message ids, add it. I'm probably the initiator of the message
+        if (!this.isIdInMessageIds(id)) {
+            this.addMessageId(id);
+        }
+
+        const broadcastMessage = JSON.stringify({
+            id: id,
+            type: 'broadcast',
+            from: `ws://localhost:${this.port}`,
+            message: message,
+        });
+
+        console.log(`Node ${this.id} - Broadcasting message: ${message}`);
+
+        this.peers.forEach((ws) => {
+            // broadcast to every one. client will check if it has already received the message
+            ws.send(broadcastMessage);
+        });
+    }
 
     // return a list of all peers (keys)
     getAllPeers() {
@@ -112,12 +155,17 @@ class Node {
         this.peers.delete(id);
     }
 
-    broadcastMessage(message) {
-        this.peers.forEach((ws, id) => {
-            // send a json message
-            ws.send(JSON.stringify(message));
-            console.log(`Sent message to ${id}: ${message}`);
-        });
+    addMessageId(id) {
+        this.messageIds.push(id);
+
+        // I think we only need to keep the last 50 message ids for this project
+        if (this.messageIds.length > 50) {
+            this.messageIds.shift();
+        }
+    }
+
+    isIdInMessageIds(id) {
+        return this.messageIds.includes(id);
     }
 
     close() {
@@ -141,103 +189,4 @@ class Node {
 
         this.serverAddresses = [];
     }
-
 }
-
-const node1 = new Node(1, 8000);
-const node2 = new Node(2, 8001);
-
-node2.connectToPeer('ws://localhost:8000');
-
-await new Promise(r => setTimeout(r, 3000));
-
-node1.connectToPeer('ws://localhost:8001');
-
-await new Promise(r => setTimeout(r, 3000));
-
-const node3 = new Node(3, 8002);
-
-node3.connectToPeer('ws://localhost:8000');
-
-await new Promise(r => setTimeout(r, 3000));
-
-
-console.log(node1.getAllPeers());
-console.log(node2.getAllPeers());
-console.log(node3.getAllPeers());
-
-await new Promise(r => setTimeout(r, 2000));
-
-node1.broadcastMessage('Hello from node1');
-
-await new Promise(r => setTimeout(r, 2000));
-
-node3.broadcastMessage('Hello from node2');
-
-await new Promise(r => setTimeout(r, 2000));
-
-//node2.close();
-node2.close();
-
-await new Promise(r => setTimeout(r, 2000));
-
-console.log(node1.getAllPeers());
-console.log(node2.getAllPeers());
-console.log(node3.getAllPeers());
-
-
-// const node3 = new Node(3, 8002);
-
-// node3.connectToPeer('ws://localhost:8000');
-
-
-// await new Promise(r => setTimeout(r, 2000));
-
-// node1.broadcastMessage('Hello from node1');
-
-// //node2.broadcastMessage('Hello from node2');
-
-
-
-// while (true) {
-//     await new Promise(r => setTimeout(r, 5000));
-//     console.log(node1.getAllPeers());
-//     console.log(node1.getAllConnections());
-// }
-
-// console.log(node1.getAllPeers());
-// console.log(node1.getAllConnections());
-
-// console.log(node2.getAllPeers());
-// console.log(node2.getAllConnections());
-
-// const node3 = new Node(3, 8002);
-
-// node3.connectToPeer('ws://localhost:8000');
-
-// await new Promise(r => setTimeout(r, 2000));
-
-// console.log(node1.getAllPeers());
-// console.log(node1.getAllConnections());
-
-// console.log(node2.getAllPeers());
-// console.log(node2.getAllConnections());
-
-// console.log(node3.getAllPeers());
-// console.log(node3.getAllConnections());
-
-
-// console.log(node3.getAllPeers());
-// console.log(node3.getAllConnections());
-
-// while (true) {
-//     await new Promise(r => setTimeout(r, 1000));
-//     console.log(node1.getAllPeers());
-// }
-
-// node1.broadcastMessage('Hello from node1');
-
-// await new Promise(r => setTimeout(r, 2000));
-
-// node2.broadcastMessage('Hello from node2');
-
